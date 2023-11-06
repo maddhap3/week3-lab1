4728f6f0a8652d3cf5b22b1dd5a034a57b661a25
vlspriya <maddhap3@gmail.com>
2023-11-06 18:12:54 +0200

    Add Static Middleware

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home Page' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { title: 'Contact US' });
+});
